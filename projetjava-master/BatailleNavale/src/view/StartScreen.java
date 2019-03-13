561154065b92ad9b743906649835f6a30c466cd7
ThomasCalonge <48481158+ThomasCalonge@users.noreply.github.com>
2019-03-13 09:47:16 +0100

    Add files via upload

---
+++ projetjava-master/BatailleNavale/src/view/StartScreen.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class StartScreen extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7178518888261894209L;
+	
+	
+	
+}
