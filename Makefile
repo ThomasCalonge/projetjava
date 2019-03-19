b4bf9baf5e1df047550dd55579ab198ede675162
Linelys <louise.beaujard@outlook.fr>
2019-03-19 16:05:45 +0100

    Fichiers textes

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 core:
-	javac -d bin src/core/*.java
+	javac -classpath bin/core -sourcepath src/core -d bin src/core/*.java
 
+app:
+	javac -classpath bin -sourcepath src -d bin src/*.java
 run:
 	@cd bin; java BatailleNavale
 
--- src/core/Radar.java
+++ src/core/Radar.java
@@ -4,11 +4,14 @@ public class Radar extends Bataille {
 
 	public Radar(final MODE mode) {
 		super(mode);
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stu
+
+
+
 	}
 
 	@Override
-	public ATTAQUE_STATUS playerAttack() {
+	public ATTAQUE_STATUS playerAttack(final Position attaxkPos) {
 		// TODO Auto-generated method stub
 		return null;
 	}
