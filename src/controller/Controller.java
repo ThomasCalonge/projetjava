0663b795f093878ccaa869a35b196733e705f3a1
ThomasCalonge <48481158+ThomasCalonge@users.noreply.github.com>
2019-03-26 10:49:24 +0100

    GameScreen_1

---
+++ src/controller/Controller.java
@@ -0,0 +1,11 @@
+package controller;
+
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import gui.*;
+import core.*;
+
+public class Controller {
+
+}
---
+++ src/gui/GameScreen.java
@@ -0,0 +1,10 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameScreen extends JFrame {
+	private static final long serialVersionUID = 8467739566752896272L;
+	
+	
+}
