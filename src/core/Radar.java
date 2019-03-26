90e458d691e101b787a3a9d55d02ebc99d528341
DridriLaBastos <adrien.cournand@gmail.com>
2019-03-26 11:34:34 +0100

    update

--- src/core/Radar.java
+++ src/core/Radar.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
-
 package core;
+
+import java.util.ArrayList;
 
 public class Radar extends Bataille {
 
@@ -21,7 +21,7 @@ public class Radar extends Bataille {
 		return false;
 	}
 
-	public void radar_reponse(final Position pos, Arraylist <Bateau> bateau_ennemi ) {
+	public void radar_reponse(final Position pos, ArrayList<Bateau> bateau_ennemi) {
 
         //Il faut l'attaque status et la position lors de l'attaque en argument
         //Par exemple lorsque l'on tire, et que l'on est en radar si on touche l'eau ça appelle cette fonction
@@ -41,27 +41,30 @@ public class Radar extends Bataille {
         for(int y = 0; y < 10; ++y){
 	        for (int x = 0; x < 10; ++x){
 				matrice[x][y] = 0;
-                if (b.o == ORIENTATION.H){
-		        	if (y == b.pos.y){
-			        	if ((x >= b.pos.x) && (x < b.pos.x + Bateau.TYPE.toInt(b.type))){
-			        		matrice[x][y] = 1;
-			        	}
-			        }
-	        	}
-                else{
-			        if (x == b.pos.x){
-				        if ((y >= b.pos.y) && (y < b.pos.y + Bateau.TYPE.toInt(b.type))){
-				        	matrice[x][y] = 1;
-				        }
-			        }
-	        	}
+				for (Bateau b: bateau_ennemi)
+				{
+					if (b.o == ORIENTATION.H){
+						if (y == b.pos.y){
+							if ((x >= b.pos.x) && (x < b.pos.x + Bateau.TYPE.toInt(b.type))){
+								matrice[x][y] = 1;
+							}
+						}
+					}
+					else{
+						if (x == b.pos.x){
+							if ((y >= b.pos.y) && (y < b.pos.y + Bateau.TYPE.toInt(b.type))){
+								matrice[x][y] = 1;
+							}
+						}
+					}
+				}
             }
         }
 
         while (found != true) {
             while (recherche){
-                for(int i = pivot_deb_x >= 0; pivot_fin_x <= 10; i++){
-                    for(int j = pivot_deb_y >= 0; pivot_fin_y <= 10; j++){
+                for(int i = pivot_deb_x; pivot_fin_x <= 10; i++){
+                    for(int j = pivot_deb_y; pivot_fin_y <= 10; j++){
                         if(matrice[i][j]==1) found = true;
                     }
                     if (pivot_deb_y > 0) pivot_deb_y--;
