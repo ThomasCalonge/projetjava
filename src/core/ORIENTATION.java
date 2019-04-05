cb2f741ce638f811dd9efab9e20bb54e76b8372c
DridriLaBastos <adrien.cournand@gmail.com>
2019-04-05 17:25:51 +0200

    update doc

--- src/core/ORIENTATION.java
+++ src/core/ORIENTATION.java
@@ -1,11 +1,15 @@
 package core;
 
 /**
- * Permet de stocker une orientation (utiliser pour les Bateaux)
+ * Permet de stocker une orientation (utiliser pour les Bateaux).<br>
+ * Une orientation est caractérisée par deux valeurs:
+ * <ul>
+ * 		<li><strong>H</strong>: littéral pour horizontale</li>
+ * 		<li><strong>V</strong>: littéral pour verticale</li>
+ * </ul>
  */
 public enum ORIENTATION
-{	/** Horizontale */
-	H,
-	/** Verticale */
-	V;
+{	
+	/** Orientation horizontale*/ H,
+	/** Orientation verticale*/   V;
 }
