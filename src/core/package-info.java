31fe53e7bef6b7c05c82b77abad1a1d818a8c782
DridriLaBastos <adrien.cournand@gmail.com>
2019-04-07 17:35:31 +0200

    update doc

--- src/core/package-info.java
+++ src/core/package-info.java
@@ -1,7 +1,7 @@
 /**
- * Package contenant les classe permettant d'implémenter la logique de la bataille navale.
- * Dans ce Package les class correspondant aux joueur, au bateaux et aux différentes batailles sont implémentées
- * ainsi que quelque enum et une class {@linkplain core.BatailleManager <b>BatailleManager</b>} permettant de facilement
- * créer une bataille.
+ * Package contenant les classes permettant d'implémenter la logique d'une bataille navale.
+ * Dans ce Package les classes correspondant aux joueurs, aux bateaux et aux différentes batailles sont implémentées
+ * ainsi que quelques enum et une class {@linkplain core.BatailleManager <b>BatailleManager</b>} permettant de facilement
+ * paramétrer et créer une bataille.
  */
 package core;
